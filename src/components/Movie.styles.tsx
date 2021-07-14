280180d39562106fe0301cc00e3b6ec9ab8af36c
Aimen <aimenhamed@live.com.au>
2021-07-14 20:27:12 +1000

    styling for hover

--- src/components/Movie.styles.tsx
+++ src/components/Movie.styles.tsx
@@ -1,11 +1,41 @@
 import styled from 'styled-components';
 
 export const MovieInfo = styled.div`
-    width: 250px;
+    width: 300px;
     margin: 1rem;
     background-color: #373b69;
+    border-radius: 3px;
+    box-shadow: 3px 3px 5px rgba(0,0,0,0.1);
+    overflow: hidden;
+    position: relative;
 `;
 
 export const MovieImage = styled.img`
     max-width: 100%;
+`;
+
+export const MovieCard = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem;
+    
+    h3 {
+        margin: 0;
+    }
+`;
+
+export const MovieOverview = styled.div`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: white;
+    color: #22254b;
+    padding: 1rem;
+    transform: translateX(100%);
+
+    ${MovieInfo}:hover & {
+        transform: translateX(0%);
+    }
 `;
--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MovieInfo, MovieImage } from "./Movie.styles";
+import { MovieInfo, MovieImage, MovieCard, MovieOverview } from "./Movie.styles";
 
 interface MovieProps {
     title: any;
@@ -12,12 +12,17 @@ const imageAPI = "https://image.tmdb.org/t/p/w1280";
 
 const Movie = (prop: MovieProps) => (
     <MovieInfo className="movie">
-         <MovieImage src={imageAPI + prop.poster_path} alt={prop.title} />
-         <div className="movie-info">
-             <h3>{prop.title}</h3>
-             <span>{prop.vote_average}</span>
-         </div>
+        <MovieImage src={imageAPI + prop.poster_path} alt={prop.title} />
+        <MovieCard>
+            <h3>{prop.title}</h3>
+            <span>{prop.vote_average}</span>
+        </MovieCard>
+        <MovieOverview>
+            <h2>Overview:</h2>
+            <p>{prop.overview}</p>
+        </MovieOverview>
     </MovieInfo>
+
 );
 
 export default Movie;
